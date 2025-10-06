71a66a2aae09f059ed9e5dfa80b0e23876e1a1c7
niktom470 <nikhil.tomar147435@gmail.com>
2025-10-06 21:29:19 +0530

    first code

---
+++ Learning/practice/index.js
@@ -0,0 +1,37 @@
+
+let hum ={
+    age: 45,
+    wt : 23,
+    ht :180,
+    greet: ()=>{
+          console.log("Hello Dunia");
+    }
+}
+
+console.log(hum.age);
+hum.greet();
+
+// const arr=[
+//     function(a,b){
+//         return a+b;
+//     },
+//     function(a,b){
+//         return a-b;
+//     },
+//     function(a,b){
+//         return a*b;
+//     },
+//     function(a,b){
+//         return a/b;
+//     },
+//     function(a,b){
+//         return a**b;
+//     }
+// ];
+
+
+// for(let i=0;i<4;i++)
+// {   let b=arr[i];
+//     let ans =b(2,4);
+//     console.log(ans);
+// }
